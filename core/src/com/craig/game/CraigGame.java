29f7da05bc395102273fc9314262d4cb43c4af50
Huw Christianson <hc1413@york.ac.uk>
2018-11-29 10:02:10 +0000

    placeholder

---
+++ core/src/com/craig/game/CraigGame.java
@@ -0,0 +1,35 @@
+package com.craig.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.craig.game.state.State;
+import com.craig.game.state.instances.Test;
+
+public class CraigGame extends Game {
+	public SpriteBatch Batch;
+
+	@Override
+	public void create () {
+		Batch = new SpriteBatch();
+		setScreen(new Test(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+
+	@Override
+	public void dispose () {
+		Batch.dispose();
+	}
+
+	/**
+	 * changes the current state
+	 * @param target state to change to
+	 */
+	public void switchState(State target)
+	{
+		setScreen(target);
+	}
+}
