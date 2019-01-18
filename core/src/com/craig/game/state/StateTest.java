65fc8573ab24c158a6c14389da4949210b26b458
Huw Christianson <hc1413@york.ac.uk>
2019-01-18 13:28:58 +0000

    junit linked and working untill intelli J decides it hates me

---
+++ core/src/com/craig/game/state/StateTest.java
@@ -0,0 +1,12 @@
+package com.craig.game.state;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StateTest {
+    @Test
+    public void test(){
+        assertNotNull(2);
+    }
+}
